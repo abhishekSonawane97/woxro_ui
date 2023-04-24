3d6e3c5cc765861aa9ebd55572f4afdceb96fd97
Abhishek <abhisheksonawane1997@gmail.com>
2023-04-24 18:42:56 +0530

    contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faComment, faPhone } from "@fortawesome/free-solid-svg-icons";
 
-const Contact = () => {
+const Contacts = () => {
   return (
-    <div>
-      contact
+    <div className="contact">
+      <div className="contactLine">
+        <p>We offer all kinds of IT services that ensure your success</p>
+      </div>
+      <div className="buttons">
+        <button className="btn btn-outline-light btn-lg m-2">
+          <a
+            href="https://chatting.page/awy0oqhfhhgorwbfeet8ywkbg4off9sl"
+            target="_blank"
+          >
+            {" "}
+            <FontAwesomeIcon icon={faComment} /> Let's Talk
+          </a>
+        </button>
+        <button className="btn btn-outline-light btn-lg m-2">
+          <a href="https://woxro.com/contactus" target="_blank">
+            {" "}
+            <FontAwesomeIcon icon={faPhone} /> Contact us
+          </a>
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contacts;
